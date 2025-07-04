96bf6063930f6b7c140430994b50ea1f9e63e23e
Kapil Jugnu <kr_kapil@hotmail.com>
2025-07-04 11:47:04 +0530

    variables and string placeholder

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
     println!("Hello, world!");
     print!("Hello, world!\n"); //alternative to println! macro, have to explicitly add new line character \n
+    let x = 10; // immutable/non changeable variable
+    let mut y = 10; // mutable/changeable variable
+    println!("variables values x={} y={}", x, y); // string with placeholder, use double quotes
+    y = 20;
+    println!("y value {}", y)
 }
