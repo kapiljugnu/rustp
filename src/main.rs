02dc5bf637fb589f964492a1326dbce15693a4b5
Kapil Jugnu <kr_kapil@hotmail.com>
2025-07-05 11:23:37 +0530

    const require types

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,7 @@ fn main() {
     let mut y = 10; // mutable/changeable variable
     println!("variables values x={} y={}", x, y); // string with placeholder, use double quotes
     y = 20;
-    println!("y value {}", y)
+    println!("y value {}", y);
+    const CONST_REQUIRE_TYPE: &str = "CONST REQUIRE TYPE"; // variable can be assign type but not mandatory
+    println!("{}", CONST_REQUIRE_TYPE);
 }
